6d9bdef26d9440ce451e0dca72ff074b85c7ff8d
sebastianchristopher <christophersebastianjohnson@gmail.com>
2019-09-13 00:08:36 +0100

    refactors todo class

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ToDo {
+
+    private int id;
+    private String content;
+    private int labelId;
+    private static ArrayList<ToDo> todos = new ArrayList<>();;
+
+    public static void add(ToDo todo){
+        todos.add(todo);
+    }
+
+    public static List<ToDo> all(){
+        return todos;
+    }
+
+    public static ToDo find(int id){
+        for(ToDo todo : todos){
+            if(todo.getId() == id){
+                return todo;
+            }
+        }
+        return null;
+    }
+
+    public static void delete(int id){
+        ToDo toDoToRemove = find(id);
+        if(find(id) != null){
+            todos.remove(toDoToRemove);
+        }
+    }
+
+    public static void edit(int id, String content){
+        ToDo toDoToEdit = find(id);
+        if(find(id) != null){
+            toDoToEdit.content = content;
+        }
+    }
+
+
+    public static  ArrayList<ToDo> filterByLabel(int labelId){
+        ArrayList<ToDo> filteredList = new ArrayList<>();
+        for(ToDo todo : todos){
+            if(todo.getLabelId() == labelId){
+                filteredList.add(todo);
+            }
+        }
+        return filteredList;
+    }
+
+    public ToDo(int id, String content, int labelId){
+        this.id = id;
+        this.content = content;
+        this.labelId = labelId;
+    }
+
+    public ToDo(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getLabelId(){
+        return labelId;
+    }
+
+}
--- src/test/java/ToDoTest.java
+++ src/test/java/ToDoTest.java
@@ -1,16 +1,81 @@
-import models.ToDo;
+import com.sun.tools.javac.comp.Todo;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 public class ToDoTest {
     public ToDo toDo;
+    ToDo toDoClass;
 
     @Before
     public void setUp() {
-        toDo = new ToDo(1, "Walk dog", 9);
+//        ToDo toDoClass = new ToDo();
+        toDo = new ToDo(1, "Walk dog", 1);
     }
+
+    // class methods
+
+    @Test
+    public void itCanAddAToDo(){
+        toDoClass.add(toDo);
+        assertThat(toDoClass.all(), contains(toDo));
+    }
+
+    @Test
+    public void itCanReturnAllToDos(){
+        ToDo toDoTwo = new ToDo(2, "Content");
+        ToDo toDoThree = new ToDo(3, "More content");
+        toDoClass.add(toDo);
+        toDoClass.add(toDoTwo);
+        toDoClass.add(toDoThree);
+
+        assertEquals(3, toDoClass.all().size());
+        assertThat(toDoClass.all(), hasItems(toDo, toDoTwo, toDoThree));
+    }
+
+    @Test
+    public void itCanReturnASpecificToDo(){
+        toDoClass.add(toDo);
+        assertEquals(toDo, toDoClass.find(1));
+    }
+
+    @Test
+    public void itCanDeleteAToDo(){
+        toDoClass.add(toDo);
+        toDoClass.delete(1);
+        assertThat(toDoClass.all(), not(contains(toDo)));
+    }
+
+    @Test
+    public void itCanEditAToDo(){
+        toDoClass.add(toDo);
+        toDoClass.edit(1, "I say something else now.");
+        String content = toDoClass.find(1).getContent();
+        assertEquals("I say something else now.", content);
+    }
+
+    @Test
+    public void itCanReturnToDosWithASpecificLabel(){
+        ToDo toDoTwo = new ToDo(2, "foo", 2);
+        ToDo toDoThree = new ToDo(3, "bar", 1);
+        toDoClass.add(toDo);
+        toDoClass.add(toDoTwo);
+        toDoClass.add(toDoThree);
+
+        ArrayList<ToDo> labelsWithOne = toDoClass.filterByLabel(1);
+
+        assertEquals(2, labelsWithOne.size());
+        assertThat(labelsWithOne, hasItems(toDo, toDoThree));
+    }
+
+    // instance methods
 
     @Test
     public void itHasAnId(){
@@ -23,7 +88,7 @@ public class ToDoTest {
     }
     @Test
     public void itCanHaveALabelId(){
-        assertEquals(9, toDo.getLabelId());
+        assertEquals(1, toDo.getLabelId());
     }
 
     @Test
@@ -32,16 +97,10 @@ public class ToDoTest {
         assertEquals(0, anotherToDo.getLabelId());
     }
 
-    @Test
-    public void theContentCanBeEdited(){
-        toDo.edit("New content");
-        assertEquals("New content", toDo.getContent());
-    }
-
-
     @After
     public void tearDown(){
         toDo = null;
+        toDoClass = null;
     }
 
 }
